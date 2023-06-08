819fd3ae795b4f0ed0cd730d56c3e96efed7eacd
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-06-08 23:15:34 +0300

    Add Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import user from './Data/user.json';
 import { Profile } from './Profile/Profile'
+import data from './Data/data.json';
+import { Statistics } from './Statistics/Statistics'
 
 export const App = () => {
   return (
@@ -11,6 +13,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
